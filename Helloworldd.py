a8867100d53d438484e2debcc07d7565d9c6c893
alhanouf96 <46896368+alhanouf96@users.noreply.github.com>
2020-08-29 18:30:40 +0300

    Add files via upload

---
+++ Helloworldd.py
@@ -0,0 +1,14 @@
+print( "Hello World") #1
+
+name = "Hanouf" #2
+print("Hello",name)
+print("Hello " +name)
+
+num = 22 #3
+print("Hello",num)
+print("Hello ",+ num)
+
+fave_food1 = "sushi" #4
+fave_food2 = "pizza"
+print("I love to eat {0} and {1}".format("sushi","pizza")) 
+f"{'I love to eat sushi and pizza'}"
